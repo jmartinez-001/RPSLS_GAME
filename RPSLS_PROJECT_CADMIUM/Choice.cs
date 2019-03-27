5dc7dd973e0477c37a74a345a7e3978cf968a7c6
jose martinez <jose.a.m089@gmail.com>
2019-03-27 11:19:42 -0500

    init

---
+++ RPSLS_PROJECT_CADMIUM/Choice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_PROJECT_CADMIUM
+{
+    class Choice
+    {
+        //member variable (Has A)
+
+
+        //constructor (Build This)
+
+
+        //member methods (Can Do)
+    }
+}
---
+++ RPSLS_PROJECT_CADMIUM/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_PROJECT_CADMIUM
+{
+    class Player
+    {
+        //member variable (Has A)
+
+
+        //constructor (Build This)
+
+
+        //member methods (Can Do)
+    }
+}
